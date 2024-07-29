852383996a827288035f1598e012833d77dbcd1d
Dmitry <79811924115@yandex.ru>
2024-07-29 16:21:39 +0300

    3. Массивы. Задания выполнены

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.every((el, i) => el == arr2[i] && arr1.length == arr2.length);
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+const getUsersNamesInAgeRange = (users, gender) => {
+  if (users.length == 0 || (gender !== 'женский' && gender !== 'мужской'))
+    return 0;
+  let filtredByGenderArr = users.filter((el) => el.gender == gender);
+  return (
+    filtredByGenderArr.reduce((sum, b) => sum + b.age, 0) /
+    filtredByGenderArr.length
+  );
+};
